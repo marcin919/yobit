408755ce0c44e0f664fb1654dab5cf490cb2c9fd
marcin919 <mar.szmyd@gmail.com>
2019-03-23 11:40:19 +0100

    Create micro.sh

---
+++ micro.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Ausgaben in Variable schreiben
+
+#Variablendefinition
+# curl -s https://yobit.net/en/trade/YOZI/BTC | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba' >> /home/yozi.txt
+
+a="$(curl -s https://yobit.net/en/trade/MICRO/BTC | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')"
+
+
+echo "MICRO/USD"  $a > /home/last_micro.txt
+
+echo $a >> /home/micro.txt
+
+mail -s"MICRO: $a" ms@mediamulti.de < /home/last_micro.txt

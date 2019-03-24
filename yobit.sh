87941f483182278bfe1572cc4284db2b5bea43d5
marcin919 <mar.szmyd@gmail.com>
2019-03-25 00:10:54 +0100

    Update yobit.sh

--- yobit.sh
+++ yobit.sh
@@ -4,17 +4,17 @@
 #Variablendefinition
 # curl -s https://yobit.net/en/trade/YOZI/BTC | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba' >> /home/yozi.txt
 
-a="$(curl -s https://yobit.net/en/trade/BTC/USD | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')"
-b="$(curl -s https://yobit.net/en/trade/AMA/BTC | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')"
-c="$(curl -s https://yobit.net/en/trade/BTT/BTC | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')"
+btc="$(curl -s https://yobit.net/en/trade/BTC/USD | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')"
+ama="$(curl -s https://yobit.net/en/trade/AMA/BTC | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')"
+btt="$(curl -s https://yobit.net/en/trade/BTT/BTC | grep "Last:" | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')"
 
 
-echo "BTC/USD"  $a > /home/last.txt
-echo "AMA/BTC"  $b >> /home/last.txt
-echo "BTT/BTC"  $c >> /home/last.txt
+echo "BTC/USD"  $btc > /home/last.txt
+echo "AMA/BTC"  $ama >> /home/last.txt
+echo "BTT/BTC"  $btt >> /home/last.txt
 
-echo $a >> /home/btc.txt
-echo $b >> /home/ama.txt
-echo $c >> /home/btt.txt
+echo $btc >> /home/btc.txt
+echo $ama >> /home/ama.txt
+echo $btt >> /home/btt.txt
 
-mail -s"Yobit" email@domain.de < /home/last_yozi.txt
+mail -s"Yobit info 0.4" email@domain.de < /home/last_yozi.txt
